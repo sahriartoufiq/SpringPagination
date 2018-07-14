30920a34e3ed18aa541a79744117704a9707d148
sahriartoufiq <sahriartoufiq@gmail.com>
2018-07-14 14:11:26 +0600

    n

--- src/main/java/com/sahriar/springPagination/controller/UserController.java
+++ src/main/java/com/sahriar/springPagination/controller/UserController.java
@@ -114,7 +114,7 @@ public class UserController {
                 user.setPicLocation(user.getPic().getOriginalFilename());
                 userService.save(user);
             } catch (Exception e) {
-                log.debug("error occured...");
+                log.debug("error occured....................");
             }
         }
 
